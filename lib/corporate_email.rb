438923ee895f725d7985fcc126cc8865daf1b2ae
Tushar <tskulkarni25@gmail.com>
2018-01-17 04:59:52 +0530

    Initial commit

---
+++ lib/corporate_email.rb
@@ -0,0 +1,5 @@
+require "corporate_email/version"
+
+module CorporateEmail
+  # Your code goes here...
+end
---
+++ spec/corporate_email_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe CorporateEmail do
+  it "has a version number" do
+    expect(CorporateEmail::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
